2dad266af32d1a5668f7a101174bd43fc3643188
EDUARDO GUTIERREZ LARIZ <eduardo.gutierrez7981@alumnos.udg.mx>
2023-08-25 12:24:21 -0600

    Agregado el componente para mostrar mensajes

---
+++ packages/frontend/src/components/Message.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <div class="justify-center h-screen d-flex align-center">
+     <div class="mb-5 text-center">
+         <h5 class="mb-1 text-h5">
+             {{ props.message }}
+         </h5>
+         <p class="text-sm">
+             {{ props.messageDescription }}
+         </p>
+ 
+         <router-link :to="props.route">
+           <v-btn 
+            icon="mdi-play"
+            size="x-large"
+            color="primary"
+            class="mt-10"></v-btn>
+         </router-link>
+ 
+     </div>
+    </div> 
+   
+ </template>
+ 
+ <script setup>
+ const props = defineProps({
+   message: {
+     type: String,
+     required: true,
+   },
+   messageDescription: {
+     type: String,
+     required: false,
+   },
+   route: {
+    type: String,
+    required: true,
+   }
+ })
+ </script>
+ 
