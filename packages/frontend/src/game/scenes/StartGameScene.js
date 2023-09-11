40ac563f840879401131db91b2591dd391952401
Ulises Ortega <ulises.ortega2937@alumnos.udg.mx>
2023-09-10 22:52:07 -0600

    Mejoras visuales

--- packages/frontend/src/game/scenes/StartGameScene.js
+++ packages/frontend/src/game/scenes/StartGameScene.js
@@ -14,12 +14,12 @@ export default class GameStartDialogScene extends Phaser.Scene {
         );
         dialogBackground.setAlpha(0.9);
 
-        // Texto
+        // Texto con una fuente adecuada
         const titleText = this.add.text(
             this.cameras.main.centerX,
             this.cameras.main.centerY - 40,
             'Bienvenido',
-            { fontSize: '24px', fill: '#000' }
+            { fontFamily: 'Arial', fontSize: '36px', fill: '#000' } // Cambia la fuente y el tamaño
         );
         titleText.setOrigin(0.5);
 
@@ -27,20 +27,31 @@ export default class GameStartDialogScene extends Phaser.Scene {
             this.cameras.main.centerX,
             this.cameras.main.centerY,
             '¿Estás listo para comenzar la tarea?',
-            { fontSize: '18px', fill: '#000', align: 'center', wordWrap: { width: 250 } }
+            { fontFamily: 'Arial', fontSize: '24px', fill: '#000', align: 'center', wordWrap: { width: 250 } } // Cambia la fuente y el tamaño
         );
         messageText.setOrigin(0.5);
 
-        // Botón
-        const startButton = this.add.text(
+        // Botón de Play (círculo)
+        const playButton = this.add.graphics();
+        playButton.fillStyle(0x00ff00, 1); // Color verde
+        playButton.fillCircle(this.cameras.main.centerX, this.cameras.main.centerY + 40, 30);
+
+        // Triángulo dentro del círculo
+        const triangle = new Phaser.Geom.Triangle(
+            this.cameras.main.centerX - 10,
+            this.cameras.main.centerY + 40 - 15,
+            this.cameras.main.centerX + 10,
+            this.cameras.main.centerY + 40 - 15,
             this.cameras.main.centerX,
-            this.cameras.main.centerY + 40,
-            'Haz click aqui', // Agregar icono de play
-            { fontSize: '20px', fill: '#000' }
+            this.cameras.main.centerY + 40 + 15
         );
-        startButton.setOrigin(0.5);
-        startButton.setInteractive();
-        startButton.on('pointerdown', () => {
+
+        playButton.fillStyle(0xffffff, 1); // Color blanco
+        playButton.fillTriangle(triangle);
+
+        // Agregar un evento al botón de Play
+        playButton.setInteractive(new Phaser.Geom.Circle(this.cameras.main.centerX, this.cameras.main.centerY + 40, 30), Phaser.Geom.Circle.Contains);
+        playButton.on('pointerdown', () => {
             this.scene.start('PlayScene'); // Cambiar a la escena principal del juego
         });
     }
