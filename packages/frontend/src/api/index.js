bae84393b4e606f1764d3d161ce160c510fc2002
Ulises Ortega <ulises.ortega2937@alumnos.udg.mx>
2023-09-12 20:19:31 -0600

    Agregada URL para WS

--- packages/frontend/src/api/index.js
+++ packages/frontend/src/api/index.js
@@ -2,13 +2,14 @@ import axios from 'axios';
 import io from 'socket.io-client';
 
 const backendUrl = process.env.VUE_APP_BACKEND_URL;
+const socketsUrl = process.env.VUE_APP_SOCKETS_URL;
 
 const api = axios.create({
     baseURL: backendUrl,
     withCredentials: true,
 });
 
-const socket = io(backendUrl, { autoConnect: false });
+const socket = io(socketsUrl, { autoConnect: false });
 
 socket.on('connect', () => {
     console.log('Conexión establecida con el servidor Socket.io');
