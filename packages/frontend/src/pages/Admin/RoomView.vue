a1ab9e1a9fae0e5148d171c8483b4b421ff20d15
EDUARDO GUTIERREZ LARIZ <eduardo.gutierrez7981@alumnos.udg.mx>
2023-09-19 23:16:04 -0600

    Actualizacion para mostrar los datos de la sala

--- packages/frontend/src/pages/Admin/RoomView.vue
+++ packages/frontend/src/pages/Admin/RoomView.vue
@@ -1,8 +1,48 @@
 <template>
     <AdminLayout>
-        <v-card>
-            {{ currentRoom }}
+        <v-card class="pa-2">
+            <v-card-title class="d-flex justify-space-between">
+                <div>
+                    <router-link to="/rooms">
+                        <v-btn size="small" icon="mdi-arrow-left-bold" color="secondary" variant="tonal"></v-btn>
+                    </router-link>
+                </div>
+                <div class="room-title">Sala: {{ currentRoom.roomName }}</div>
+            </v-card-title>
+            <v-divider></v-divider>
+            <v-card-text>
+                <!-- {{ currentRoom }} -->
+
+                <v-list-item title="Número de sala" :subtitle="currentRoom.roomNumber"
+                    @click="copyText(currentRoom.roomNumber)">
+                    <template v-slot:append>
+                        <v-icon icon="mdi-content-copy"></v-icon>
+                    </template>
+                </v-list-item>
+
+                <v-list-item title="Fecha de creación" :subtitle="currentRoom.createdAt"></v-list-item>
+                <v-list-item title="Fecha de expiración" :subtitle="currentRoom.expiration"></v-list-item>
+                <v-list-item title="Tiempo maximo de la prueba:" :subtitle="currentRoom.maxTime + ' min'"></v-list-item>
+                <v-list-item title="Inicio rapido" :subtitle="currentRoom.quickStart"></v-list-item>
+                <v-list-item title="Configuración de la sala" :subtitle="currentRoom.status"></v-list-item>
+
+                <div class="text-h6">Tiempo maximo de la prueba:</div>
+                <div class="text-subtitle-1"> {{ currentRoom.maxTime }} min</div>
+
+                <div class="text-h6">Personas en la sala:</div>
+                <v-chip prepend-icon="mdi-account" v-for="player in currentRoom.players" color="primary">
+                    {{ player }}
+                </v-chip>
+            </v-card-text>
         </v-card>
+        <v-snackbar v-model="copySnackbar" timeout="2000">
+            Número de sala copiado
+            <template v-slot:actions>
+                <v-btn color="blue" variant="text" @click="copySnackbar = false">
+                    Cerrar
+                </v-btn>
+            </template>
+        </v-snackbar>
     </AdminLayout>
 </template>
 
@@ -23,4 +63,19 @@ onMounted(async () => {
     }
 })
 
+const copySnackbar = ref(false);
+
+const copyText = (roomNumber) => {
+    navigator.clipboard.writeText(roomNumber);
+    copySnackbar.value = true;
+}
+
 </script>
+
+<style scoped>
+.room-title{
+    color: #424242;
+    /*font-weight: 500 !important;*/
+    margin-right: 15px;
+}
+</style>
